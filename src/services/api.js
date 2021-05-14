dfe44d2d44ff1560abc08a91be68911f2e49aaba
isTorres <ismaeltorres612@gmail.com>
2021-05-14 17:04:54 -0300

    add function to get category and name of product from API

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ export async function getCategories() {
   // Implemente aqui
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const response = await request.json();
+  return response;
 }
