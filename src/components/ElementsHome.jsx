5c67278a9cd6d62c1ba082e75dbbc381dbbed597
André Arnoni <andre_arnoni@yahoo.com.br>
2021-05-14 18:56:07 -0300

    resolve the second requiriment

---
+++ src/components/ElementsHome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ElementsHome extends Component {
+    render() {
+        return (
+            <div>
+                <input
+                type="text"
+                />
+            </div>
+        )
+    }
+}
