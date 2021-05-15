8a4e532b5de5c858f45b362684b686c84366ab7c
André Arnoni <andre_arnoni@yahoo.com.br>
2021-05-14 21:23:29 -0300

    fix git issues

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <div>
-      <header className="App-header">
+      <header>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Home } />
-            {/* <Route exact path="/ShoppingCart" component={ ShoppingCart } /> */}
+            <Route exact path="/shoppingcart" component={ ShoppingCart } />
             {/* <Route component={ NotFound } /> */}
           </Switch>
         </BrowserRouter>
