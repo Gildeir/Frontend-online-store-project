fd4858b6aafc14787d8c41d34f24596f174f190d
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-05-16 13:50:05 -0300

    Requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ElementsHome from '../components/ElementsHome';
+import Categories from '../components/Categories';
 
 export default class Home extends Component {
   constructor(props) {
@@ -9,12 +10,14 @@ export default class Home extends Component {
   }
 
   render() {
+    // const { categories } = this.state;
     return (
       <div>
         <ElementsHome />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Categories />
       </div>
     );
   }
