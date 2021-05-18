4c131c315407b1a106ba34e3062f64338ec25a01
André Arnoni <andre_arnoni@yahoo.com.br>
2021-05-18 20:24:08 -0300

    adding

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,11 +3,48 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default class ProductDetails extends Component {
+constructor(props) {
+  super(props)
+
+  this.handleClickAddCart2 = this.handleClickAddCart2.bind(this)
+
+  this.state = {}
+}
+  
+
+  handleClickAddCart2 = (product) => {
+    const { location: { state } } = this.props;
+    const { cart } = state;
+    const haveCart = cart.length;
+    if (!haveCart) {
+      const { id, title } = product;
+      const productCart = [{ id, title, count: 1 }];
+      this.setState({
+        cart: productCart,
+      });
+    } else {
+      let productCart = cart;
+      const findProduct = productCart.find((data) => data.id === product.id);
+      if (findProduct) {
+        const key = productCart.indexOf(findProduct);
+        productCart[key].count += 1;
+        this.setState({
+          cart: productCart,
+        });
+      } else {
+        const { id, title } = product;
+        productCart = [...productCart, { id, title, count: 1 }];
+        this.setState({
+          cart: productCart,
+        });
+      }
+    }
+  }
+
   render() {
     const { location: { state } } = this.props;
-    const { product, cart } = state;
+    const { product } = state;
     const { title, price, thumbnail } = product;
-    const { handleClickAddCart } = this.props;
     
     return (
       <div>
@@ -16,15 +53,14 @@ export default class ProductDetails extends Component {
           <img src={ thumbnail } alt={ title } />
           <h2>{`Preço: R$ ${price}`}</h2>
         </div>
-        <button type="button">
-          <Link
-            to={ { pathname: '/shoppingcart', state: { cart } } }
-            data-testid="product-detail-add-to-cart"
-            onClick={ () => handleClickAddCart(product) }
-          >
-            <p>ADICIONAR AO CARRINHO</p>
-          </Link>
-        </button>
+        <button
+          type="button"
+          value={ product.id }
+          onClick={ () => this.handleClickAddCart2(product) }
+          data-testid="product-add-to-cart"
+        >
+          Adicionar ao Carrinho
+            </button>
         <button type="button">
           <Link to="/">
             Voltar
